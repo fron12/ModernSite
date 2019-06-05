5733ced908dc197c26cef1d0281ce144476defbe
Fred <sohnf90@yahoo.com>
2019-06-05 00:41:51 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="top-header">
+        <img className="logo" src="https://d3v5ezchayoty7.cloudfront.net/wp-content/uploads/2017/12/05005748/On-Q-Logo-Tagline-Optimized.png" alt="Company Name" />
+        <div className="header-button-group">
+          <div className="blue-button button-properties">Purchase or Refinance</div>
+          <div className="orange-button button-properties">Find a Location</div>
+        </div>
       </header>
+      <div className="dropdown-categories">
+        
+      </div>
     </div>
   );
 }
