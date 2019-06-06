764fe6af255c832863f30ad96c1f4e4d26c80fa7
Fred <sohnf90@yahoo.com>
2019-06-06 01:00:54 -0700

    Add code for shrinking dropdown menues when scrolled past a certain point for later. Add image and text.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faGlobe, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
-function App() {
+class App extends Component {
+  state = {
+    position: window.pageYOffset
+  }
+  componentDidMount(){
+    console.log(this.state.position);
+  }
+  render(){
   return (
-    <div className="App">
+    <div className="container">
       <header className="top-header">
         <img className="logo" src="https://d3v5ezchayoty7.cloudfront.net/wp-content/uploads/2017/12/05005748/On-Q-Logo-Tagline-Optimized.png" alt="Company Name" />
         <div className="header-button-group">
@@ -15,15 +22,27 @@ function App() {
         </div>
       </header>
       <section className="dropdown-categories">
-        <a href="*">About on Q <FontAwesomeIcon icon={faAngleDown} /></a>
-        <a href="*">Home Loans <FontAwesomeIcon icon={faAngleDown} /></a>
-        <a href="*">Financial Tools <FontAwesomeIcon icon={faAngleDown} /></a>
-        <a href="*">Mortage Education <FontAwesomeIcon icon={faAngleDown} /></a>
-        <a href="*">Mortage News</a>
-        <a href="*"><FontAwesomeIcon icon={faGlobe} /> EN <FontAwesomeIcon icon={faAngleDown} /></a>
+        <a className="dropdown-main" href="*">About on Q <FontAwesomeIcon icon={faAngleDown} /></a>
+        <a className="dropdown-main" href="*">Home Loans <FontAwesomeIcon icon={faAngleDown} /></a>
+        <a className="dropdown-main" href="*">Financial Tools <FontAwesomeIcon icon={faAngleDown} /></a>
+        <a className="dropdown-main" href="*">Mortage Education <FontAwesomeIcon icon={faAngleDown} /></a>
+        <a className="dropdown-main" href="*">Mortage News</a>
+        <a className="dropdown-main" href="*"><FontAwesomeIcon icon={faGlobe} /> EN <FontAwesomeIcon icon={faAngleDown} /></a>
+        <FontAwesomeIcon icon={faSearch} />
+      </section>
+      <section className="couch-with-bulldog">
+        <div className="image-words">
+          <div>Mortage Simplified.</div>
+          <p>Get prequalified or started ona  loan application or refinance.</p>
+          <div className="image-button-group">
+            <div className="blue-button button-properties">Purchase</div>
+            <div className="blue-button button-properties">Refinance</div>
+          </div>
+        </div>
       </section>
     </div>
   );
+  }
 }
 
 export default App;
