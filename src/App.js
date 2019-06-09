6115cf0a430ced57ad84f4ceac55644482c947ba
Fred <sohnf90@yahoo.com>
2019-06-09 00:37:03 -0700

    Add size change for the dropdown menus when scrolled past a certain point. Added scroll event handler

--- src/App.js
+++ src/App.js
@@ -6,11 +6,30 @@ import { faAngleDown, faGlobe, faSearch } from '@fortawesome/free-solid-svg-icon
 
 class App extends Component {
   state = {
-    position: window.pageYOffset
+    position: 0,
+    size: 'normal'
   }
-  componentDidMount(){
-    console.log(this.state.position);
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.scrollHandler);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.scrollHandler);
+  }
+
+  scrollHandler = () => {
+    let style = document.getElementsByClassName("dropdown-categories")[0].style;
+    if(document.body.scroll > 120 || document.documentElement.scrollTop > 120){
+      style.height = "57px";
+      style.lineHeight = "57px";
+    }
+    else{
+      style.height = "77px";
+      style.lineHeight = "77px";
+    }
+  }
+
   render(){
   return (
     <div className="container">
@@ -25,19 +44,33 @@ class App extends Component {
         <a className="dropdown-main" href="*">About on Q <FontAwesomeIcon icon={faAngleDown} /></a>
         <a className="dropdown-main" href="*">Home Loans <FontAwesomeIcon icon={faAngleDown} /></a>
         <a className="dropdown-main" href="*">Financial Tools <FontAwesomeIcon icon={faAngleDown} /></a>
-        <a className="dropdown-main" href="*">Mortage Education <FontAwesomeIcon icon={faAngleDown} /></a>
-        <a className="dropdown-main" href="*">Mortage News</a>
+        <a className="dropdown-main" href="*">Mortgage Education <FontAwesomeIcon icon={faAngleDown} /></a>
+        <a className="dropdown-main" href="*">Mortgage News</a>
         <a className="dropdown-main" href="*"><FontAwesomeIcon icon={faGlobe} /> EN <FontAwesomeIcon icon={faAngleDown} /></a>
         <FontAwesomeIcon icon={faSearch} />
       </section>
-      <section className="couch-with-bulldog">
+      <section className="main-content">
+        <div className="couch-with-bulldog"></div>
         <div className="image-words">
-          <div>Mortage Simplified.</div>
-          <p>Get prequalified or started ona  loan application or refinance.</p>
+          <h1 className="mortgages-simplified image-words-color">
+            Mortgages
+            <br/>
+            Simplified.
+          </h1>
+          <h2 className="get-prequalified image-words-color">
+            Get prequalified or started on a loan
+            <br/>
+            application or refinance.
+          </h2>
           <div className="image-button-group">
-            <div className="blue-button button-properties">Purchase</div>
-            <div className="blue-button button-properties">Refinance</div>
+            <div className="blue-button image-button-properties">Purchase</div>
+            <div className="blue-button image-button-properties">Refinance</div>
           </div>
+        </div>
+      </section>
+      <section className="review-scores">
+        <div className="score-group">
+          <img src="https://d3v5ezchayoty7.cloudfront.net/wp-content/uploads/2017/04/03230746/Google_Widget_v3-07.png" alt="google review" />
         </div>
       </section>
     </div>
